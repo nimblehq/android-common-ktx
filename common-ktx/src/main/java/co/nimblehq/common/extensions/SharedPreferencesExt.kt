6cdc891d2924e16d1a4a687089c391ac99ad8084
Duy Oscar <ntduy@indivisys.jp>
2021-11-08 15:06:41 +0700

    [#8] Add Set/Get/ClearAll for SharedPreferences extension

---
+++ common-ktx/src/main/java/co/nimblehq/common/extensions/SharedPreferencesExt.kt
@@ -0,0 +1,64 @@
+package co.nimblehq.common.extensions
+
+import android.content.*
+import android.content.Context.MODE_PRIVATE
+import com.google.gson.Gson
+
+val Context.myAppPreferences: SharedPreferences
+    get() = getSharedPreferences(
+        "${this.packageName}_${this.javaClass.simpleName}",
+        MODE_PRIVATE
+    )
+
+inline fun <reified T : Any> SharedPreferences.getObject(key: String): T? {
+    return Gson().fromJson<T>(getString(key, null), T::class.java)
+}
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : Any> SharedPreferences.get(key: String, defaultValue: T? = null): T {
+    return when (T::class) {
+        Boolean::class -> getBoolean(key, defaultValue as? Boolean? ?: false) as T
+        Float::class -> getFloat(key, defaultValue as? Float? ?: 0.0f) as T
+        Int::class -> getInt(key, defaultValue as? Int? ?: 0) as T
+        Long::class -> getLong(key, defaultValue as? Long? ?: 0L) as T
+        String::class -> getString(key, defaultValue as? String? ?: "") as T
+        else -> {
+            if (defaultValue is Set<*>) {
+                getStringSet(key, defaultValue as Set<String>) as T
+            } else {
+                val typeName = T::class.java.simpleName
+                throw Error("Unable to get shared preference with value type '$typeName'. Use getObject")
+            }
+        }
+    }
+}
+
+
+@Suppress("UNCHECKED_CAST")
+inline operator fun <reified T : Any> SharedPreferences.set(key: String, value: T) {
+    with(edit()) {
+        when (T::class) {
+            Boolean::class -> putBoolean(key, value as Boolean)
+            Float::class -> putFloat(key, value as Float)
+            Int::class -> putInt(key, value as Int)
+            Long::class -> putLong(key, value as Long)
+            String::class -> putString(key, value as String)
+            else -> {
+                if (value is Set<*>) {
+                    putStringSet(key, value as Set<String>)
+                } else {
+                    val json = Gson().toJson(value)
+                    putString(key, json)
+                }
+            }
+        }
+        commit()
+    }
+}
+
+fun SharedPreferences.clearAll() {
+    with(edit()) {
+        clear()
+    }
+}
+
