4126b87fa2908ba46a8a1a062d4e230a2cd7d0b3
doandinh <doan@nimblehq.co>
2021-11-11 16:06:29 +0700

    [#8] Update unit test on `toDate` function

--- common-ktx/src/test/java/co/nimblehq/common/extensions/DateExtensionsTest.kt
+++ common-ktx/src/test/java/co/nimblehq/common/extensions/DateExtensionsTest.kt
@@ -332,8 +332,8 @@ class DateExtensionsTest {
     @Test
     fun toDate() {
         assertEquals(
-            Date().with(year = 1987, month = 6, day = 2, hour = 0, minute = 0, second = 0),
-            "1987-06-02".toDate("yyyy-MM-dd")
+            Date().with(year = 1987, month = 6, day = 2, hour = 0, minute = 0, second = 0).toString(),
+            "1987-06-02".toDate("yyyy-MM-dd").toString()
         )
     }
 }
