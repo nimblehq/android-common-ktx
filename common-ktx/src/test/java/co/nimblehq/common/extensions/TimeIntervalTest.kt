1f3fcfedc96b65151d4a55d7e13ef9cbb4a09dbb
doandinh <doan@nimblehq.co>
2021-11-11 15:13:26 +0700

    [#7] Add Date extension and UT

---
+++ common-ktx/src/test/java/co/nimblehq/common/extensions/TimeIntervalTest.kt
@@ -0,0 +1,28 @@
+package co.nimblehq.common.extensions
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import java.util.Calendar
+
+/**
+ * Unit test for [TimeInterval].
+ */
+class TimeIntervalTest {
+
+    @Test
+    fun ago() {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, -1)
+        val yesterday = calendar.time
+        assertEquals(yesterday, 1.days.ago)
+    }
+
+    @Test
+    fun later() {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.YEAR, 5)
+        val fiveYearsLater = calendar.time
+        assertEquals(fiveYearsLater, 5.years.forward)
+    }
+}
+
