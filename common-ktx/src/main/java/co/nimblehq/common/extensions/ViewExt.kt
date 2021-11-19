ea90853f1bad96e0207795cd407d93e277a4a191
Lydiasama <hallelujah.lydia@gmail.com>
2021-11-19 15:06:30 +0700

    [#2] Add comments

--- common-ktx/src/main/java/co/nimblehq/common/extensions/ViewExt.kt
+++ common-ktx/src/main/java/co/nimblehq/common/extensions/ViewExt.kt
@@ -9,18 +9,43 @@ import androidx.annotation.DimenRes
 private const val COLLAPSED_STATE_ROTATION = 180f
 private const val EXPANDED_STATE_ROTATION = 0f
 
+/**
+* Make view visible.
+*/
 fun View.visible() {
     this.visibility = View.VISIBLE
 }
 
+/**
+ * Make view invisible.
+ */
 fun View.invisible() {
     this.visibility = View.INVISIBLE
 }
 
+/**
+ * Make view gone.
+ */
 fun View.gone() {
     this.visibility = View.GONE
 }
 
+/**
+ * Check if the visible condition is true; set view visible; otherwise, set view gone.
+ *
+ * @param visible The condition to make view visible.
+ */
+fun View.visibleOrGone(visible: Boolean) {
+    if (visible) visible() else gone()
+}
+
+/**
+ * Check if the visible condition is true; set view visible; otherwise, set view gone.
+ * The visibility will active after the delay.
+ *
+ * @param visible The condition to make view visible.
+ * @param delay The delay time in millisecond to affect the view.
+ */
 fun View.visibleOrGone(visible: Boolean, delay: Long) {
     if (delay <= 0L) {
         visibleOrGone(visible)
@@ -29,44 +54,77 @@ fun View.visibleOrGone(visible: Boolean, delay: Long) {
     }
 }
 
-fun View.visibleOrGone(visible: Boolean) {
-    if (visible) visible() else gone()
-}
-
+/**
+ * Check if the visible condition is true; set view visible; otherwise, set view invisible.
+ *
+ * @param visible The condition to make view visible.
+ */
 fun View.visibleOrInvisible(visible: Boolean) {
     if (visible) visible() else invisible()
 }
 
+/**
+ * Check if the view is visible then invoke the listener.
+ *
+ * @param listener The function would be called after checking view is visible.
+ */
 inline fun View.ifVisibleThen(listener: (View) -> Unit) {
     if (this.visibility == View.VISIBLE) {
         listener.invoke(this)
     }
 }
 
+/**
+ * Set bottom padding.
+ *
+ * @param dimenRes The dimension resource id.
+ */
 fun View.setBottomPaddingRes(@DimenRes dimenRes: Int) {
     setPadding(paddingLeft, paddingTop, paddingRight, resources.getDimensionPixelSize(dimenRes))
 }
 
+/**
+ * Set top padding.
+ *
+ * @param dimenRes The dimension resource id.
+ */
 fun View.setTopPaddingRes(@DimenRes dimenRes: Int) {
     setPadding(paddingLeft, resources.getDimensionPixelSize(dimenRes), paddingRight, paddingBottom)
 }
 
-fun View.setTopPadding(padding: Int) {
-    setPadding(paddingLeft, padding, paddingRight, paddingBottom)
-}
-
+/**
+ * Enable view when the condition is matched.
+ *
+ * @param predicate The condition to enable the view.
+ */
 fun View.enableWhen(predicate: () -> Boolean) {
     this.isEnabled = predicate()
 }
 
+/**
+ * Convert pixel to Sp for Font sizing usage.
+ *
+ * @param px The pixel amount.
+ */
 fun View.convertPxToSp(px: Int): Float {
     return px / resources.displayMetrics.scaledDensity
 }
 
+/**
+ * Convert Sp to pixel.
+ *
+ * @param sp The Sp amount.
+ */
 fun View.convertSpToPx(sp: Float): Int {
     return (sp * resources.displayMetrics.scaledDensity).toInt()
 }
 
+/**
+ * Return bitmap with specific size.
+ *
+ * @param height Height of bitmap result.
+ * @param width Width of bitmap result.
+ */
 fun View.getBitmap(height: Int, width: Int): Bitmap {
     val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(bitmap)
