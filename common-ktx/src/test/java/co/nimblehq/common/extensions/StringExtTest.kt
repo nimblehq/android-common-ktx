42e95ac661cc9465797a97d25205b1a40dd32fae
Lydiasama <hallelujah.lydia@gmail.com>
2021-11-23 17:23:05 +0700

    [#4] Update assertion

--- common-ktx/src/test/java/co/nimblehq/common/extensions/StringExtTest.kt
+++ common-ktx/src/test/java/co/nimblehq/common/extensions/StringExtTest.kt
@@ -10,56 +10,56 @@ class StringExtTest {
     fun `When given a string, isNotNullOrEmpty should return true`() {
         val given = "given string"
         val result = given.isNotNullOrEmpty()
-        assertEquals(true, result)
+        assertTrue(result)
     }
 
     @Test
     fun `When given a null string, isNotNullOrEmpty should return false`() {
         val given: String? = null
         val result = given.isNotNullOrEmpty()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Test
     fun `When given a empty string, isNotNullOrEmpty should return false`() {
         val given = ""
         val result = given.isNotNullOrEmpty()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Test
     fun `When given a blank space, isNotNullOrEmpty should return true`() {
         val given = " "
         val result = given.isNotNullOrEmpty()
-        assertEquals(true, result)
+        assertTrue(result)
     }
 
     @Test
     fun `When given a string, isNotNullOrBlank should return true`() {
         val given = "given string"
         val result = given.isNotNullOrBlank()
-        assertEquals(true, result)
+        assertTrue(result)
     }
 
     @Test
     fun `When given a null string, isNotNullOrBlank should return false`() {
         val given: String? = null
         val result = given.isNotNullOrBlank()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Test
     fun `When given a empty string, isNotNullOrBlank should return false`() {
         val given = ""
         val result = given.isNotNullOrBlank()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Test
     fun `When given a blank space, isNotNullOrBlank should return false`() {
         val given = " "
         val result = given.isNotNullOrBlank()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Test
@@ -77,19 +77,19 @@ class StringExtTest {
     @Test
     fun `When given a string in email format, it returns true`() {
         val result = "email@email.com".isEmailValid()
-        assertEquals(true, result)
+        assertTrue(result)
     }
 
     @Test
     fun `When given a string lacks of @, it returns false`() {
         val result = "emailemail.com".isEmailValid()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Test
     fun `When given a string lacks of the top-level domains, it returns false`() {
         val result = "email@email".isEmailValid()
-        assertEquals(false, result)
+        assertFalse(result)
     }
 
     @Ignore
