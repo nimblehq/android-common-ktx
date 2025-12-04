78cca0aee50df8cab0c3bc1fc07b810aa4e5fa43
Zaw Myo Naing (Rahul) <zawmyo@nimblehq.co>
2025-12-04 16:27:19 +0700

    [#52] Update the deprecated `kotlinOptions` to the new `compilerOptions` syntax for setting the JVM target in the `app` and `common-ktx` modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -33,8 +35,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
     }
 }
 
